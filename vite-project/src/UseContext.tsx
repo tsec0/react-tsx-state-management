289ebea215c8150b57d6b6f39b5f2125c087d6d4
Tsvetomir Dikov <46728923+tsec0@users.noreply.github.com>
2024-08-21 16:47:37 +0300

    Update UseContext.tsx

--- vite-project/src/UseContext.tsx
+++ vite-project/src/UseContext.tsx
@@ -5,6 +5,7 @@ import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { Link, Outlet, ReactLocation, Router, useMatch } from '@tanstack/react-location';
 
 const queryClient = new QueryClient();
+const location = new ReactLocation();
 
 function SearchBox(){
   const { search, setSearch } = usePokemon();
