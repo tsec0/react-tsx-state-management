7fd968a8bbb5ef9013024491dd3e8d97ae8a5c6d
Tsvetomir Dikov <46728923+tsec0@users.noreply.github.com>
2024-08-22 09:14:51 +0300

    Starter project and updated Context for pokemon project

--- vite-project/src/main.tsx
+++ vite-project/src/main.tsx
@@ -1,6 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import App from './UseContext.tsx'
+import App from './Starter.tsx'
 import './index.css';
 
 createRoot(document.getElementById('root')!).render(
